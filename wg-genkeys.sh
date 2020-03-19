92a82b41e64cfaa166d402b16e4b59c5ab7b3f10
Paweł Szubert <gimbus109@gmail.com>
2020-03-19 10:08:44 +0100

    Bugfixes

--- wg-genkeys.sh
+++ wg-genkeys.sh
@@ -19,13 +19,12 @@ echo ${private_key} > ${output}/${server}/private
 echo ${public_key} > ${output}/${server}/public
 server_public_key=${public_key}
 echo > ${output}/${server}/${server}.conf
-vpnip=${vpnip} private_key=${private_key} port=${port} envsubst < ${servertemplate} >> ${output}/${server}/${server}.conf
+serverip=${vpnip}${n} private_key=${private_key} port=${port} envsubst < ${servertemplate} >> ${output}/${server}/${server}.conf
 
 #peers
 while read line; do
   i=$line
   n=$((n+1))
-  vpnip=10.10.0.$n
   private_key=$(wg genkey)
   public_key=$(echo $private_key | wg pubkey)
   preshared_key=$(wg genkey)
@@ -34,9 +33,9 @@ while read line; do
   echo ${private_key} > ${output}/${i}/private
   echo ${public_key} > ${output}/${i}/public
   echo ${preshared_key} > ${output}/${i}/preshared
-  vpnip=${vpnip} peer=${i} public_key=${public_key} preshared_key=${preshared_key} localnet=${localnet} \
+  peerip=${vpnip}${n} peer=${i} public_key=${public_key} preshared_key=${preshared_key} localnet=${localnet} \
     envsubst < ${peersectiontemplate} >> ${output}/${server}/${server}.conf
-  vpnip=${vpnip} peer=${i} server_public_key=${server_public_key} preshared_key=${preshared_key} localnet=${localnet} private_key=${private_key}\
+  peerip=${vpnip}${n} peer=${i} server_public_key=${server_public_key} preshared_key=${preshared_key} localnet=${localnet} private_key=${private_key}\
   endpoint=${endpoint} port=${port}  envsubst < ${peertemplate} > ${output}/${i}/${i}.conf
   qrencode -t ansiutf8 < ${output}/${i}/${i}.conf > ${output}/${i}/${i}.qrcode
   qrencode -o  ${output}/${i}/${i}.png < ${output}/${i}/${i}.conf
